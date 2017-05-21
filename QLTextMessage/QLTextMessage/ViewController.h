157fa263cb5702a4e46dc1c845f6f62b4cb2277d
MQL-IT <mengqingling@MQLing.local>
2017-05-21 19:37:28 +0800

    导入开源库

---
+++ QLTextMessage/QLTextMessage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QLTextMessage
+//
+//  Created by MQL-IT on 2017/5/21.
+//  Copyright © 2017年 MQL-IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QLTextMessage/QLTextMessage/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  QLTextMessage
+//
+//  Created by MQL-IT on 2017/5/21.
+//  Copyright © 2017年 MQL-IT. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
+
+
+@end
